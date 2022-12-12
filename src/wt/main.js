3a8ea869385f55d9ed12fc8870d0d0cb344a001f
dmitry_levkovich <dzmitry.liaukovich@gmail.com>
2022-12-12 13:53:27 +0400

    feat: add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
+const numberCpus = os.cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  const workersArray = []
+
+  for (let i = 0; i < numberCpus; i++) {
+    let number = 10 + i;
+    const worker = new Worker('./worker.js', { workerData: { num: number } });
+    workersArray.push(worker);
+  }
+  for (const [index, worker] of workersArray.entries()) {
+    let number = 10;
+    worker.once("message", result => {
+      console.log(`${number + index}th Fibonacci No: ${result}`);
+    });
+
+    worker.on("error", error => {
+      console.log(error);
+    });
+
+    worker.on("exit", exitCode => {
+      // console.log(`It exited with code ${exitCode}`);
+    })
+  }
 };
 
 await performCalculations();
